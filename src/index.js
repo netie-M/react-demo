8482d8d04e5dc992365caa7d2d2fbb32ba4edc2c
yanyanmao <YanyanMao@quarkfinance.com>
2017-03-22 11:34:31 +0800

    commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import  React from 'react';
+import  ReactDOM from 'react-dom';
+import HelloMessage from './components/HelloMessage.js';
+ReactDOM.render(<div>
+    <HelloMessage name="Jhon"/>
+</div>, document.getElementById("content"));
+
+
+
